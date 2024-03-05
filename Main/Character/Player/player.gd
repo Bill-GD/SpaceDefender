5b386b69dfa1fd0d4b49fb656ee49908fa9d77bb
Bill-GD <116184478+Bill-GD@users.noreply.github.com>
2024-03-05 16:40:02 +0700

    Renamed main folder

--- Main/Character/Player/player.gd
+++ Main/Character/Player/player.gd
@@ -4,9 +4,10 @@ extends Character
 func _physics_process(delta) -> void:
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
 	velocity = direction * basic_stats.speed
-	move_and_slide()
+	move_and_collide(velocity * delta)
 
 	look_at(get_global_mouse_position())
 
-func _input(event: InputEvent) -> void:
-	print(event.as_text())
+#func _input(event: InputEvent) -> void:
+	#if event.is_action_pressed("primary_attack"):
+		#print(event.as_text())
